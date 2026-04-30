e7c6a8f20e365f3c34568b6b5990826abd53c255
yassine <chebourouyassine05@gmail.com>
2026-04-30 19:37:48 +0100

    chore: overwrite local backend folder with main branch version

--- Backend/fittech/fitapi/urls.py
+++ Backend/fittech/fitapi/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import (
     # Auth
+    AssignCoachView,
     CoachReviewDetailView,
     CoachReviewListCreateView,
     CoachCertificateListCreateView,
